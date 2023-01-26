2004d9d1c6c6001bf8d7df9a3a14bab451b193a0
Qubisted <78350166+Qubisted@users.noreply.github.com>
2023-01-26 11:40:21 +0000

    fix: port forwarding

--- pcconfig.py
+++ pcconfig.py
@@ -3,8 +3,7 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="pynecone_starter",
-    api_url="pynecone-starter-production.up.railway.app",
-    bun_path="$HOME/.bun/bin/bun",
+    port=3945,
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
 )
