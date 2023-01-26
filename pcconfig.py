9c88e1f42f74934cb3310e615b4514bec2de2c47
Qubisted <78350166+Qubisted@users.noreply.github.com>
2023-01-26 11:30:10 +0000

    fix: server link?

--- pcconfig.py
+++ pcconfig.py
@@ -3,6 +3,8 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="pynecone_starter",
+    api_url="pynecone-starter-production.up.railway.app",
+    bun_path="$HOME/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.DEV,
 )
